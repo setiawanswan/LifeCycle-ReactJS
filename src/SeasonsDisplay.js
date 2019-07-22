979bdbf4932026fb3d97824ac26d342812c94433
setiawanswan <setiawan.tumilin@gmail.com>
2019-07-22 14:20:19 +0700

    lifecycle method

--- src/SeasonsDisplay.js
+++ src/SeasonsDisplay.js
@@ -1,26 +1,38 @@
 import React from 'react';
+import './SeasonDisplay.css';
+
+const SeasonsDisplay = props => {
+    const season = getSeason(props.lat, new Date().getMonth());
+    const {text, iconName } = seasonConfig[season];
+
+    return (
+        <div className={`season-display ${season}`}>
+            <i className={`icon-left massive ${iconName} icon`} />
+                {text}
+            <i className={`icon-right massive ${iconName} icon`} />
+        </div>
+    ); 
+};
+
+const seasonConfig = {
+    summer: {
+        text: 'lets hit the beach',
+        iconName: 'sun'
+    },
+    winter: {
+        text: 'Burr it is cold',
+        iconName: 'snowflake'
+    }
+};
 
 const getSeason = (lat, month) => {
     if (month > 2 && month < 9){
-        return lat > 0 ? 'Summer' : 'Winter';
+        return lat > 0 ? 'summer' : 'winter';
     } else {
-        return lat > 0 ? 'Winter' : 'Summer';
+        return lat > 0 ? 'winter' : 'summer';
     }
 };
 
 // Ternary Expression 
-const SeasonsDisplay = props => {
-    const season = getSeason(props.lat, new Date().getMonth());
-    const text = season === 'winter' ? 'Burr, it is chilly' : 'Lets hit the beach'; 
-    const icon = season === 'winter' ? 'snowflake' : 'sun';
-
-    return (
-        <div>
-            <i className={`${icon} icon`} />
-            {text}
-            <i className={`${icon} icon`} />
-        </div>
-    ); 
-};
 
 export default SeasonsDisplay;
